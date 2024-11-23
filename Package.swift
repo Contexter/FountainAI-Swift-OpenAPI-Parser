de91b2c5cd749ff3248ddabd8695272737d28b6b
Benedikt Eickhoff <mail@benedikt-Eickhoff.de>
2024-11-23 19:29:15 +0100

    Verified and finalized resource paths in Package.swift

--- Package.swift
+++ Package.swift
@@ -24,17 +24,15 @@ let package = Package(
             path: "Tests/OpenAPIParserTests",
             resources: [
                 .process("Sources/OpenAPIParserLib/OpenAPI/Action-Service.yml"),
-                .process("Sources/OpenAPIParserLib/OpenAPI/Story-Factory-Service.yml"),
+                .process("Sources/OpenAPIParserLib/OpenAPI/Central-Sequence-Service.yml"),
+                .process("Sources/OpenAPIParserLib/OpenAPI/Character-Service.yml"),
                 .process("Sources/OpenAPIParserLib/OpenAPI/Core-Script-Managment-Service.yml"),
                 .process("Sources/OpenAPIParserLib/OpenAPI/Ensemble-Service.yml"),
+                .process("Sources/OpenAPIParserLib/OpenAPI/Paraphrase-Service.yml"),
                 .process("Sources/OpenAPIParserLib/OpenAPI/Performer-Service.yml"),
-                .process("Sources/OpenAPIParserLib/OpenAPI/Central-Sequence-Service.yml"),
                 .process("Sources/OpenAPIParserLib/OpenAPI/Session-And-Context-Service.yml"),
                 .process("Sources/OpenAPIParserLib/OpenAPI/Spoken-Word-Service.yml"),
-                .process("Sources/OpenAPIParserLib/OpenAPI/Paraphrase-Service.yml"),
-                .process("Sources/OpenAPIParserLib/OpenAPI/Character-Service.yml"),
-                .process("Tests/OpenAPIParserTests/sample.yaml"),
-                .process("Tests/OpenAPIParserTests/sample.json")
+                .process("Sources/OpenAPIParserLib/OpenAPI/Story-Factory-Service.yml")
             ]
         ),
     ]
